25f0de8298b233d369dbee5299702b21f453ad9b
Умар <45699126+Godikbot@users.noreply.github.com>
2021-02-12 21:58:10 +0500

    Update com.py

--- idm/my_signals/com.py
+++ idm/my_signals/com.py
@@ -10,45 +10,45 @@ from microvk import VkApi
 def little_theft(event: MySignalEvent) -> str:
     if event.args[0] != 'беседа': return "ok"
     event.msg_op(2, """все команды начинаются с ".с"
-    .с реши
-    .с боты
-    .с люди
-    .с имя
-    .с ад
-    .с учас
-    .с беседа""")
+.с реши
+.с боты
+.с люди
+.с имя
+.с ад
+.с учас
+.с беседа""")
     return "ok"
     
 @dp.longpoll_event_register('п')
 @dp.my_signal_event_register('п')
 def little_theft(event: MySignalEvent) -> str:
-    if event.args[0] != 'простойдеж': return "ok"
+    if event.args[0] != 'простой': return "ok"
     event.msg_op(2, """
-    .с кража ав 
-    .с пуши
-    .с алло
-    .с ксмс
-    .с Рестарт
-    .с тест
-    .с взлом жопы
-    .с опрос
-    .с спам
-    .с время
-    .с прочитать все
-    .с свалить
-    .с связать
-    .с повтори
-    .с статус
-    .с бот
-    .с кто
-    .с ж
-    .с мессага""")
+.с кража ав 
+.с пуши
+.с алло
+.с ксмс
+.с Рестарт
+.с тест
+.с взлом жопы
+.с опрос
+.с спам
+.с время
+.с прочитать все
+.с свалить    
+.с связать
+.с повтори
+.с статус
+.с бот
+.с кто
+.с ж
+.с мессага""")
     return "ok"
 
 dp.longpoll_event_register('п')
 @dp.my_signal_event_register('п')
 def little_theft(event: MySignalEvent) -> str:
-    if event.args[0] != 'бетадеж': return "ok"
+    if event.args[0] != 'бета': return "ok"
     event.msg_op(2, """
 .с проф
 .с кража ав 
