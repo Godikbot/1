737bf6d029dc4788f30a34573e3579910be9d8bd
Умар <45699126+Godikbot@users.noreply.github.com>
2021-02-13 09:08:06 +0500

    Update prefixes.py

--- idm/longpoll_signals/prefixes.py
+++ idm/longpoll_signals/prefixes.py
@@ -1,30 +1,24 @@
 from idm.objects import dp, LongpollEvent, MySignalEvent
 
-
+@dp.my_signal_event_register('+префикс', '-префикс')
 @dp.longpoll_event_register('+префикс', '-префикс')
 def binds_info(event: LongpollEvent):
     event.msg_op(2, 'ℹ️ Для добавления биндов используй префиксы лп модуля ' +
                  '(по умолчанию ".лп", "!лп")')
 
-
+@dp.my_signal_event_register('префиксы')
 @dp.longpoll_event_register('префиксы')
 def binds_list(event: LongpollEvent):
     prefixes = event.db.lp_settings['prefixes']
     if not prefixes:
         message = ('Я не знаю как ты этого достиг, но у тебя нет ни одного ' +
-                   'LP префикса. На всякий случай добавил префикс "!л", ' +
+                   'LP префикса. На всякий случай добавил префикс .п", ' +
                    'можешь пользоваться им (возможно понадобится ' +
                    'перезапуск LP модуля)')
-        event.db.lp_settings['prefixes'].append('!л')
+        event.db.lp_settings['prefixes'].append('.п')
         event.db.save()
     else:
         message = 'Префиксы LP сигналов:'
         for prefix in prefixes:
             message += f'\n-- "{prefix}"'
     event.msg_op(2, message)
-
-
-@dp.my_signal_event_register('+префикс', '-префикс')
-def prefixes_callback_info(event: MySignalEvent):
-    event.msg_op(2, 'ℹ️ Менять префиксы можно только через LP модуль\n' +
-                 'https://github.com/Elchinchel/ICAD-Longpoll')
