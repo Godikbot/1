a1f3b3a4d6a14825a9edc4f06c74b695323b4ff1
Умар <45699126+Godikbot@users.noreply.github.com>
2021-07-29 18:50:19 +0500

    Update ping.py

--- idm/callback_signals/signals/ping.py
+++ idm/callback_signals/signals/ping.py
@@ -11,15 +11,4 @@ def ping(event: SignalEvent) -> str:
     else "ТОК" if event.command == "тик" else "КОНГ")
 
     event.send(event.responses['ping_duty'].format(время = delta, ответ = r_type))
-    time.sleep(20)
-    event.api.msg_op(3, msg_id=message_id)
-    return "ok"
-
-
-@dp.signal_event_register('привет')
-def ping(event: SignalEvent) -> str:
-    c_time = datetime.now().timestamp()
-    delta = round(c_time - event.msg['date'], 2)
-
-    event.send(event.responses['1'])
     return "ok"
