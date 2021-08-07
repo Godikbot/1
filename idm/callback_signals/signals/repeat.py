e3efd086656cad757b319aeccc7f5b5514f2ebb2
Умар <45699126+Godikbot@users.noreply.github.com>
2021-08-07 10:25:13 +0500

    Update repeat.py

--- idm/callback_signals/signals/repeat.py
+++ idm/callback_signals/signals/repeat.py
@@ -7,16 +7,16 @@ def repeat(event: SignalEvent) -> str:
     if event.msg['from_id'] not in event.db.trusted_users:
         message_id = event.send(event.responses['not_in_trusted'])
         time.sleep(3)
-        event.api.msg_op(1, msg_id=message_id)
+        event.api.msg_op(3, msg_id=message_id)
         return "ok"
-   
+
     msg = event.payload.lower()
     for word in event.responses['repeat_forbidden_words']:
         if word in msg or (msg.startswith('!') and not msg.startswith('!с')):
             event.send(event.responses['repeat_if_forbidden'])
             return "ok"
 
-    message = f'​{" ".join(event.args[0:])}​{event.payload}​'
+    message = f'{" ".join(event.args[1:])}\n{event.payload}'
     if message == '\n':
         event.send('А че написать-то?')
     else:
