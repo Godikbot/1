69c52124f2d87734f94eaf88d7c6a0611ba81406
Умар <45699126+Godikbot@users.noreply.github.com>
2021-10-14 23:16:02 +0500

    Простая обновка
    Ничего важного
    .д н 
    Напишет любое слово но на них боты не смогут ответить

--- idm/callback_signals/signals/repeat.py
+++ idm/callback_signals/signals/repeat.py
@@ -16,7 +16,7 @@ def repeat(event: SignalEvent) -> str:
             event.send(event.responses['repeat_if_forbidden'])
             return "ok"
 
-    message = f' {" ".join(event.args[2:])} \n{event.payload} '
+    message = f'⁬⁬⁬{" ".join(event.args[1:])}⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬⁬'
     if message == '\n':
         event.send('А че написать-то?')
     else:
